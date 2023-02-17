bb81795f8089a0cad65b226144a2d2488f553232
moshushi <moshushi@gmail.com>
2023-02-17 23:34:31 +0300

    add marks

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,10 +1,14 @@
+import pytest
+
 from src.summator import summator
 
 
+@pytest.mark.smoke
 def test_one():
     assert summator(10, 20) == 30
 
 
+@pytest.mark.skip
 def test_two():
-    # summator(-10, -4) == -14
+    summator(-10, -4) == -14
     assert summator(-10, -4) == 30
