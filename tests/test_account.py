9aad057dc2696c31ed4483b5ed887468cf893f15
moshushi <moshushi@gmail.com>
2023-02-19 02:44:27 +0300

    add first fixure

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,24 +1,33 @@
 """
 Проверяем корректность создания аккаунта
 """
+import pytest
+
 from src.account import Account
 
 
-def test_account_create_balance():
+@pytest.fixture
+def default_account():
+    return Account("Nakamoto", 0)
+
+
+def tear_down(account):
+    del account
+
+
+def test_account_create_balance(default_account):
     """
     Проверка баланса счёта
     :return:
     """
-    a = Account("Nakamoto", 0)
-    assert a.balance == 0
-    del a
+    assert default_account.balance == 0
+    # del default_account
 
 
-def test_account_create_name():
+def test_account_create_name(default_account):
     """
     Проверка баланса счёта
     :return:
     """
-    a = Account("Nakamoto", 0)
-    assert a.owner == "Nakamoto"
-    del a
+    assert default_account.owner == "Nakamoto"
+    # del default_account
