7b210ddf9e84db4023512ea5ea2c889dcb003645
moshushi <moshushi@gmail.com>
2023-02-22 23:00:36 +0300

    add decorator for benchmark

--- examples/decorator_for_benchmark.py
+++ examples/decorator_for_benchmark.py
@@ -3,13 +3,14 @@
 """
 import time
 
-def benchmark(func):
 
+def benchmark(func):
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        t = end -start
+        t = end - start
         print("Время выполнения:", t)
         return result
+
     return wrapper
