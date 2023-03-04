6f41da3409aa255292d7462f2b4178d6181cb407
moshushi <moshushi@gmail.com>
2023-03-05 00:16:14 +0300

    try to mark failed test

---
+++ marks/test_example_one.py
@@ -0,0 +1,13 @@
+"""Пример маркировки проваленных тестов"""
+import pytest
+
+@pytest.mark.regress
+@pytest.mark.skip(reason="Mantis-337")
+def test_example_one():
+    """Тест который всегда проваливается"""
+    assert 0
+@pytest.mark.regress
+@pytest.mark.xfail(strict=True)
+def test_example_two():
+    """Тест который всегда проваливается"""
+    assert False
