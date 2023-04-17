647f672516802f2f62da727ff1bb9fe6603c278b
moshushi <moshushi@gmail.com>
2023-04-18 02:16:36 +0300

    some pylint

--- parametrization/test_parametrization.py
+++ parametrization/test_parametrization.py
@@ -1,7 +1,9 @@
+"""Тестирование параметризации"""
+
 import pytest
-import requests
+
 
 # Example with single parameter
-@pytest.mark.parametrize("param", [1,2,3,4])
+@pytest.mark.parametrize("param", [1, 2, 3, 4])
 def test_one(param):
     assert param % 2 == 0
