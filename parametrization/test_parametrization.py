53e24bfbeecf05fe8fee87c33ed2abc23028e00f
moshushi <moshushi@gmail.com>
2023-06-16 00:37:17 +0300

    new commit

--- parametrization/test_parametrization.py
+++ parametrization/test_parametrization.py
@@ -7,3 +7,21 @@ import pytest
 @pytest.mark.parametrize("param", [1, 2, 3, 4])
 def test_one(param):
     assert param % 2 == 0
+
+
+# Example with several parameter
+@pytest.mark.parametrize("param1, param2", [
+    (1, 2),
+    (3, 4),
+    (5, 6),
+    (7, 8)
+])
+def test_two(param1, param2):
+    assert (param1 + param2) % 3 == 0
+
+
+# Example with nested parametrization
+@pytest.mark.parametrize("param2", [1, 2, 3, 4, 5])
+@pytest.mark.parametrize("param1", [6, 7, 8, 9, 0])
+def test_three_nested(param1, param2):
+    assert (param1 + param2) % 2 == 0
